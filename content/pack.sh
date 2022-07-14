1773e9a3fbc6e3808ab94e649876943508ff81d8
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-07-14 16:05:57 -0400

    Testing -t option

--- content/pack.sh
+++ content/pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pushd content
-docker run --rm -v $(pwd):/nasher -v $(pwd):"/root/.local/share/Neverwinter Nights/$2/" urothis/nwnee-community-images:nasher-8193.34 pack --clean --verbose --yes $2
+docker run --rm -t -v $(pwd):/nasher -v $(pwd):"/root/.local/share/Neverwinter Nights/$2/" urothis/nwnee-community-images:nasher-8193.34 pack --clean --verbose --yes $2
 popd
