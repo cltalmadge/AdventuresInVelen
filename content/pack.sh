26e3a66dde0107e1a4297354f701f9bc797128a0
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-07-14 15:52:00 -0400

    Forgot shebang

--- content/pack.sh
+++ content/pack.sh
@@ -1,3 +1,4 @@
-﻿pushd content
+﻿#!/bin/bash
+pushd content
 docker run --rm -it -v $(pwd):/nasher -v $(pwd):"/root/.local/share/Neverwinter Nights/$2/" urothis/nwnee-community-images:nasher-8193.34 unpack --clean --verbose $2
 popd
