87df0c6b8ac1c845f62c21954b786147fd9305df
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-07-13 21:35:57 -0400

    Added nasher.cfg to content

---
+++ mod/unpack_mod_windows.bat
@@ -0,0 +1,2 @@
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 unpack --file AdventuresInVelen.mod --default"
+echo Finished.
