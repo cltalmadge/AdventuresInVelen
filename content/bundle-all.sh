71c08cb023f2b112a7629b52844d28a7130a2ebb
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-07-14 17:16:07 -0400

    Added ability to bundle all.

---
+++ content/bundle-all.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+pushd content/ || exit
+mkdir velen_content/
+cp ./*.tlk velen_content
+cp ./*.hak velen_content
+zip -r velen_content.zip velen_content/
+rm -rf velen_content
+rm ./*.tlk
+rm ./*.hak
+popd || exit
