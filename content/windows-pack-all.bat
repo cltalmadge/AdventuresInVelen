6da6cfe6730f7424bb030e190fa0c0c9a01f86d2
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-07-13 23:32:24 -0400

    Testing jenkins pipeline

---
+++ content/windows-pack-all.bat
@@ -0,0 +1,29 @@
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_top"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_tiles8"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_tiles7"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_tiles6"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_tiles5"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_tiles4"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_tiles3"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_tiles2"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_tiles1"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_robe"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_plcs"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_cont"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_ccoh"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose velen_anim"
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose tlk"
---
+++ mod/pack_mod_windows.bat
@@ -0,0 +1,2 @@
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 pack --verbose --clean"
+echo Finished.
