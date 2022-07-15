a01b44513568ebb908a282044bdc91083badc23a
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-07-15 12:25:56 -0400

    Added install-mod.sh

---
+++ content/install-mod.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+# get dir from user
+if [ $# -eq 0 ]; then
+    echo "No directory specified."
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+# Set the directory to install to.
+DIR=$1
+# Make sure the directory isn't root.
+if [ "$DIR" == "/" ]; then
+    echo "Cannot install to root."
+    exit 1
+fi
+
+# pushd mod/
+pushd mod/ || exit
+# Run pack-module.sh to create the mod file.
+./pack-module.sh
+# move mod to dir
+mv ./*.mod "$DIR"
+# popd mod/
+popd || exit
