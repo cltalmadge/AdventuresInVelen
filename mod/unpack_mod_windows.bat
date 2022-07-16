f2789d2ea85a15686ac8ad3f2ee57fcc79488661
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-07-16 15:37:44 -0400

    Added haks as dependencies for module

--- mod/unpack_mod_windows.bat
+++ mod/unpack_mod_windows.bat
@@ -1,2 +1,2 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 unpack --file AdventuresInVelen.mod --default"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "docker run --rm -it -v ${pwd}:/nasher urothis/nwnee-community-images:nasher-8193.34 unpack --file CapeVelen.mod --default"
 echo Finished.
