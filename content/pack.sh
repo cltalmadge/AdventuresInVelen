a5bcb8c4192c56bf7dc3b5b0fee3ce785f8a03a0
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-07-16 11:37:17 -0400

    Fixed typo in content/pack.sh

--- content/pack.sh
+++ content/pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pushd content
-docker run --rm -t -v $(pwd):/nasher -v $(pwd):"/root/.local/share/Neverwinter Nights/$2/" urothis/nwnee-community-images:nasher-8193.34 pack --clean --verbose --yes $2
-popd
+pushd content || exit
+docker run --rm -t -v $(pwd):/nasher -v $(pwd):"/root/.local/share/Neverwinter Nights/$1/" urothis/nwnee-community-images:nasher-8193.34 pack --clean --verbose --yes $2
+popd || exit
