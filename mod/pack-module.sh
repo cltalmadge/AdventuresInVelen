29db3af953cc1435c9d116d831997d9f9037a5c8
Caelan L. Talmadge <34071000+cltalmadge@users.noreply.github.com>
2022-07-16 11:16:36 -0400

    Added option to reset server after module builds.

--- mod/pack-module.sh
+++ mod/pack-module.sh
@@ -1,1 +1,6 @@
-﻿docker run --rm -t -v $(pwd):/nasher urothis/nwnee-community-images:nasher-8193.34 pack --clean --verbose --yes
+﻿#!/bin/bash
+# Get the directory the script file is in.
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$SCRIPTDIR" || exit
+docker run --rm -t -v $(pwd):/nasher urothis/nwnee-community-images:nasher-8193.34 pack --clean --verbose --yes
+popd || exit
---
+++ mod/reset-server.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/bash
+pushd /home/zoltan/velen_mod || exit
+docker-compose down
+docker-compose up -d
+popd || exit
