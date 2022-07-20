a14325fcaf0ea1273c14055f5d1c64cd457ae7d0
ctalmadge <talmadge12@gmail.com>
2022-07-20 14:18:02 -0400

    Implemented LevelProvider.SetLevel

--- src/Velen/Velen.Leveling/Services/LevelProvider.cs
+++ src/Velen/Velen.Leveling/Services/LevelProvider.cs
@@ -9,7 +9,7 @@ namespace Velen.Leveling.Services;
 public sealed class LevelProvider : ILevelValueProvider
 {
     public int GetLevel(VelenPlayer player) => NWScript.GetHitDice(player.LoginObjectId);
-    public void SetLevel(VelenPlayer player, int level)
-    {
-    }
+
+    public void SetLevel(VelenPlayer player, int level) =>
+        NWScript.SetXP(player.LoginObjectId, level * (level - 1) * 500);
 }
